c14eae6b6afb4927cf027d6ec8bf7cfdcd0a5c2d
m4tu987 <m.maturana1999@gmail.com>
2024-08-30 13:03:16 -0400

    Se agrega movimiento
    Se agrega el script de movimiento y un label al player del server

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,25 +1,42 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var speed = 400.0
+var acceleration = 300
+var dash_speed = 300
+var gravity = 400
 
+@onready var nombre_personaje = $Nombre_personaje
 
+var player 
+
+func _input(event):
+	if is_multiplayer_authority():
+		if event.is_action_pressed("test"):
+			test.rpc()
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if is_multiplayer_authority():
+		var move_input = Input.get_axis("move_left","move_right")
+		velocity.x = move_toward(velocity.x, speed * move_input, acceleration * delta)
+		var move_input2 = Input.get_axis("move_up","move_down")
+		velocity.y = move_toward(velocity.y, speed * move_input2, acceleration * delta)
+		if Input.is_action_just_pressed("dash"):
+			velocity.x = (dash_speed * velocity.x)/100
 	move_and_slide()
+	send_position.rpc(position)
+	
+func setup(player_data):
+	name = str(player_data.id)
+	set_multiplayer_authority(player_data.id)
+	nombre_personaje.text = player_data.name
+	player = player_data
+	
+@rpc("authority","call_local","unreliable")
+func test():
+	Debug.log("test %s" % player.name)
+	
+@rpc()	
+func send_position(pos):
+	position = pos
+	
+	
