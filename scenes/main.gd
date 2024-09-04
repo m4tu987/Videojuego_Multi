489a123fba18a754ecaa0170ed482515522bd791
m4tu987 <m.maturana1999@gmail.com>
2024-09-04 19:35:04 -0400

    Se instancian correctamente los players en la escena main

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+@export var player_scene : PackedScene
+@onready var Players = $Players
+
+func _ready() -> void:
+	for player_data in Game.players:
+		var player_inst = player_scene.instantiate()
+		Players.add_child(player_inst)
+		player_inst.setup(player_data)
