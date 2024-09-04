4e3d056f1dad53e7cc1aa9b37c2052416b6029a1
m4tu987 <m.maturana1999@gmail.com>
2024-09-04 19:47:46 -0400

    Se cambia la posición de los personajes

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,9 +2,13 @@ extends Node2D
 
 @export var player_scene : PackedScene
 @onready var Players = $Players
+@onready var markers = $Markers
 
 func _ready() -> void:
-	for player_data in Game.players:
+	for i in Game.players.size():
+		var player_data = Game.players[i]
 		var player_inst = player_scene.instantiate()
 		Players.add_child(player_inst)
 		player_inst.setup(player_data)
+		player_inst.global_position = markers.get_child(i).global_position
+		
