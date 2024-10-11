d2028899c5ab08a22391a02cf1c1a16a09c7b137
VicenteLepe <vicente.lepe.c@ug.uchile.cl>
2024-10-11 12:58:35 -0300

    add: map loop

--- scenes/map/camera_2d.gd
+++ scenes/map/camera_2d.gd
@@ -3,6 +3,6 @@ extends Camera2D
 
 func _physics_process(delta):
 	if Input.is_action_pressed("left"):
-		position.x -= 300*delta
+		position.x -= 200*delta
 	if Input.is_action_pressed("right"):
-		position.x += 300*delta
+		position.x += 200*delta
