5cef66856bc0fb0da29402f266bfb94ac577ad4a
VicenteLepe <vicente.lepe.c@ug.uchile.cl>
2024-10-16 20:29:43 -0300

    Camera autoscroll

--- scenes/map/camera_2d.gd
+++
@@ -1,8 +0,0 @@
-extends Camera2D
-
-
-func _physics_process(delta):
-	if Input.is_action_pressed("left"):
-		position.x -= 200*delta
-	if Input.is_action_pressed("right"):
-		position.x += 200*delta
