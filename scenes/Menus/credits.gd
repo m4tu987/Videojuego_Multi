72d0b9b83b460feffb1000b1fe07d59c678c33e7
m4tu987 <m.maturana1999@gmail.com>
2024-11-22 13:17:01 -0300

    escena de creditos

---
+++ scenes/Menus/credits.gd
@@ -0,0 +1,12 @@
+extends MarginContainer
+
+@onready var button = $PanelContainer/MarginContainer/VBoxContainer/Button
+@onready var animation_player = $Parallax2D/Sprite2D/AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	button.pressed.connect(_on_back_pressed)
+	animation_player.play("new_animation")
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/Menus/main_menu.tscn")
