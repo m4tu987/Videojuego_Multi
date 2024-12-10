0d28606ce8470ba3335ed1dd0154050f9053e596
m4tu987 <m.maturana1999@gmail.com>
2024-12-09 22:45:10 -0300

    Update victory_text.gd

--- scenes/Menus/victory_text.gd
+++ scenes/Menus/victory_text.gd
@@ -1,10 +1,10 @@
 extends MarginContainer
+@export var lobby_player_scene: PackedScene
 @onready var back_menu = $Panel/MarginContainer/VBoxContainer/Back_menu
 @onready var victory_text = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().paused = false
 	back_menu.pressed.connect(_back_menu)
 
 func _back_menu():
