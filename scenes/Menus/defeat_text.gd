1caeb30c0d43e0892954dbb3fb63f77dd0b45a40
m4tu987 <m.maturana1999@gmail.com>
2024-12-09 22:36:54 -0300

    arreglo de errores

--- scenes/Menus/defeat_text.gd
+++ scenes/Menus/defeat_text.gd
@@ -1,6 +1,7 @@
 extends MarginContainer
 @export var lobby_player_scene: PackedScene
 @onready var back_to_menu = $Panel/MarginContainer/VBoxContainer/Back_to_menu
+@onready var defeat_text = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,5 +13,6 @@ func _back_to_menu():
 	multiplayer.multiplayer_peer.close()
 	Game.players = []
 	get_tree().paused = false
+	defeat_text.visible = false
 	get_tree().change_scene_to_file("res://scenes/Menus/main_menu.tscn")
 	
--- scenes/Menus/victory_text.gd
+++ scenes/Menus/victory_text.gd
@@ -1,5 +1,6 @@
 extends MarginContainer
 @onready var back_menu = $Panel/MarginContainer/VBoxContainer/Back_menu
+@onready var victory_text = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,4 +11,5 @@ func _back_menu():
 	multiplayer.multiplayer_peer.close()
 	Game.players = []
 	get_tree().paused = false
+	victory_text.visible = false
 	get_tree().change_scene_to_file("res://scenes/Menus/main_menu.tscn")
